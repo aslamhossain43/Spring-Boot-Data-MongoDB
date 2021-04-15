87c3cabe12763f4188c32c348ff440b53444a774
Aslam-Hossain <aslamecehstu43java@gmail.com>
2021-04-15 17:23:42 +0600

    Initial commit

---
+++ Spring-Boot-Data-MongoDB/src/test/java/com/data/mongo/main/SpringBootDataMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.mongo.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
